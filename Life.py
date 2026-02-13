e44ee5bf35fde9e39b44d83e5aa020a62e619453
nothing positive <165131116+nothing032@users.noreply.github.com>
2026-02-13 06:14:47 -0800

    Update Life.py

--- Life.py
+++ Life.py
@@ -1,1 +1,15 @@
+import babase
+import bauiv1 as bui
+import bascenev1 as bs
+import weakref
+from bauiv1lib import party
+from bauiv1lib.popup import PopupWindow, PopupMenuWindow
 
+version = '1.0.0'
+bs.screenmessage("hi")
+
+# ba_meta require api 9
+# ba_meta export babase.Plugin
+
+class Mod(babase.Plugin):
+    pass
