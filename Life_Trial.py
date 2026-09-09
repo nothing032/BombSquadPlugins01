58c97327812273bd7fe0726d47ed3c4905b6cc60
nothing positive <165131116+nothing032@users.noreply.github.com>
2026-09-09 05:53:42 -0700

    Update Life_Trial.py

--- Life_Trial.py
+++ Life_Trial.py
@@ -1,7 +1,7 @@
 try:import os,re,urllib.request,babase as A,baenv
 except:pass
-H='https://raw.githubusercontent.com/nothing032/BombSquadPlugins01/main/WonderBomb_api8.py'
-I='https://raw.githubusercontent.com/nothing032/BombSquadPlugins01/main/WonderBomb_api9.py'
+H='https://raw.githubusercontent.com/nothing032/BombSquadPlugins01/main/Life_api8.py'
+I='https://raw.githubusercontent.com/nothing032/BombSquadPlugins01/main/Life_api9.py'
 def B():
 	G='error'
 	try:
@@ -9,7 +9,7 @@ def B():
 		if[1,7,20]<=C<=[1,7,36]:D=H
 		elif C>=[1,7,37]:D=I
 		else:A.screenmessage('Your game version is not supported.',color=(1,0,0));A.getsimplesound(G).play();return
-		E=A.env()['python_directory_user'];B='Wonder.py';F=[O for O in os.listdir(E)if re.match('^Wonder(\\s*\\(\\d+\\))?\\.py$',O,re.I)]
+		E=A.env()['python_directory_user'];B='Life.py';F=[O for O in os.listdir(E)if re.match('^Life(\\s*\\(\\d+\\))?\\.py$',O,re.I)]
 		if F:B=min(F,key=len)
 		L=os.path.join(E,B)
 		with urllib.request.urlopen(D,timeout=15)as M:N=M.read()
