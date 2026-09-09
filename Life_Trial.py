a864356485cf9829b090c680bbdf5697838fc6a2
nothing positive <165131116+nothing032@users.noreply.github.com>
2026-09-09 05:32:50 -0700

    Update Life_Trial.py

--- Life_Trial.py
+++ Life_Trial.py
@@ -1,15 +1,15 @@
 try:import os,re,urllib.request,babase as A,baenv
 except:pass
-H='https://raw.githubusercontent.com/nothing032/BombSquadPlugins01/main/Life_api8.py'
-I='https://raw.githubusercontent.com/nothing032/BombSquadPlugins01/main/Life_api9.py'
+H='https://raw.githubusercontent.com/nothing032/BombSquadPlugins01/main/WonderBomb_api8.py'
+I='https://raw.githubusercontent.com/nothing032/BombSquadPlugins01/main/WonderBomb_api9.py'
 def B():
 	G='error'
 	try:
-		J=baenv.TARGET_BALLISTICA_VERSION;K=re.sub('[a-zA-Z].*','',J);C=[int(A)for A in K.split('.')]
+		J=baenv.TARGET_BALLISTICA_VERSION;K=re.sub('[a-zA-Z].*','',J);C=[int(O)for O in K.split('.')]
 		if[1,7,20]<=C<=[1,7,36]:D=H
 		elif C>=[1,7,37]:D=I
 		else:A.screenmessage('Your game version is not supported.',color=(1,0,0));A.getsimplesound(G).play();return
-		E=A.env()['python_directory_user'];B='Life.py';F=[A for A in os.listdir(E)if re.match('^Life(\\s*\\(\\d+\\))?\\.py$',A,re.I)]
+		E=A.env()['python_directory_user'];B='Wonder.py';F=[O for O in os.listdir(E)if re.match('^Wonder(\\s*\\(\\d+\\))?\\.py$',O,re.I)]
 		if F:B=min(F,key=len)
 		L=os.path.join(E,B)
 		with urllib.request.urlopen(D,timeout=15)as M:N=M.read()
