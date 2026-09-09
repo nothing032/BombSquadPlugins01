beb610cd52c6fd45e621ec85a9b74cd7a11bcebf
nothing positive <165131116+nothing032@users.noreply.github.com>
2026-09-09 05:28:31 -0700

    Create Wonder_dev.py

---
+++ Wonder_dev.py
@@ -0,0 +1,19 @@
+try:import os,re,urllib.request,babase as A,baenv
+except:pass
+H='https://raw.githubusercontent.com/nothing032/BombSquadPlugins01/main/WonderBomb_api8.py'
+I='https://raw.githubusercontent.com/nothing032/BombSquadPlugins01/main/WonderBomb_api9.py'
+def B():
+	G='error'
+	try:
+		J=baenv.TARGET_BALLISTICA_VERSION;K=re.sub('[a-zA-Z].*','',J);C=[int(A)for A in K.split('.')]
+		if[1,7,20]<=C<=[1,7,36]:D=H
+		elif C>=[1,7,37]:D=I
+		else:A.screenmessage('Your game version is not supported.',color=(1,0,0));A.getsimplesound(G).play();return
+		E=A.env()['python_directory_user'];B='Wonder.py';F=[A for A in os.listdir(E)if re.match('^Life(\\s*\\(\\d+\\))?\\.py$',A,re.I)]
+		if F:B=min(F,key=len)
+		L=os.path.join(E,B)
+		with urllib.request.urlopen(D,timeout=15)as M:N=M.read()
+		with open(L,'wb')as O:O.write(N)
+		A.screenmessage(f"Installed successfully: {B}. Please restart the game.",color=(0,1,0));A.getsimplesound('ding').play()
+	except Exception as P:A.screenmessage(f"Installation failed: {P}",color=(1,0,0));A.getsimplesound(G).play()
+B()
